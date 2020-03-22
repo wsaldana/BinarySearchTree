150767783dfd3fd70284ceea03f87aa95cc9b4c7
wsaldana <53311124+wsaldana@users.noreply.github.com>
2020-03-21 19:46:13 -0600

    Creación del programa principal

---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructuras de Datos
+ * @author Walter Saldaña #19897
+ * @since 21 de marzo del 2020
+ * 
+ * Modelo de traductor Inglés-Español implementado en un Binary Search Tree
+ */
+
+public class Main{
+    
+    public static void main(String[] args){
+
+    }
+}
